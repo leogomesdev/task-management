18b4c3ce61271b442ea4359588da546921bb3613
Leonardo Gomes <leonardo.gdsilva@amedigital.com>
2020-07-28 21:16:55 -0300

    change CORS for prod and dev environments

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger: Logger = new Logger('bootstrap');
+  const serverConfig = config.get('server');
 
   const app: INestApplication = await NestFactory.create(AppModule);
 
-  if (process.env.NODE_ENV === 'development') {
-    app.enableCors();
-  }
+  app.enableCors({ origin: serverConfig.origin });
+  logger.log(`Accepting requests from origin ${serverConfig.origin}`);
 
-  const port = process.env.PORT || config.get('server').port;
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application is running on port ${port}`);
 }
